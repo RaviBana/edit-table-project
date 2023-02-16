824aee1442d893b4aa8150270f2eb6326a46d435
Ravindra <ravindrasingh0506@gmail.com>
2023-02-16 16:06:15 -0400

    last commit

--- Content.js
+++ Content.js
@@ -13,93 +13,72 @@ function Content() {
     const Input = [
         {
             name: 'OnBoarding call',
-            value: 'ravi',
-            setValue: 'setValue1'
         },
         {
             name: 'Google Search Console Access',
-            value: '',
-            setValue: 'setValue2'
+           
         },
         {
             name: 'Google Analytics Access',
-            value: '',
-            setValue: 'setValue3'
+           
         },
         {
             name: 'Website Access',
-            value: '',
-            setValue: 'setValue4'
+           
         },
         {
             name: 'Technical Audit',
-            value: '',
-            setValue: 'setValue5'
+        
         },
         {
             name: 'Anchor Text Semantic Analysis',
-            value: '',
-            setValue: 'setValue6'
+        
         },
         {
             name: 'Competitor Analysis',
-            value: '',
-            setValue: 'setValue7'
+        
         },
         {
             name: 'Anchor Text/URL Mapping',
-            value: '',
-            setValue: 'setValue8'
+        
         },
         {
             name: 'Google Data Studio Report + Local Reporting Suite',
-            value: '',
-            setValue: 'setValue9'
+        
         },
         {
             name: 'Site Level Optimization',
-            value: '',
-            setValue: 'setValue10'
         },
         {
-            name: 'On Page Optimisation',
-            value: '',
-            setValue: 'setValue11'
-        },
+            name : 'On Page Optimisation',
+           
+    },
         {
             name: 'Content Creation',
-            value: '',
-            setValue: 'setValue12'
+           
         },
         {
             name: 'Content Publishing',
-            value: '',
-            setValue: 'setValue13'
+           
         },
         {
             name: 'Premium Press Release',
-            value: '',
-            setValue: 'setValue14'
+           
         },
         {
             name: 'Authority Niche Placements',
-            value: '',
-            setValue: 'setValue15'
+           
         },
         {
             name: 'Review Management',
-            value: '',
-            setValue: 'setValue16'
+           
         },
         {
             name: 'Index Links',
-            value: '',
-            setValue: 'setValue17'
+           
         },
         {
             name: 'Vedio Recamp',
-            value: '',
-            setValue: 'setValue18'
         }
     ]
 
@@ -189,7 +168,7 @@ function Content() {
 
                                     <div id="formInput">
                                         <label htmlFor={e.name} id='label'>{e.name} :-</label>
-                                        <input required type="text" name={e.name} value={data[e.name]} id={e.name} onChange={(E) => sentValue(E.target.value, e.name, e)} />
+                                        <input required type="text" name={e.name} value={data[e.name]} id={e.name} placeholder = 'Enter..' onChange={(E) => sentValue(E.target.value, e.name, e)} />
                                     </div>
 
                                 )
