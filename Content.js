e91c015c41100e587dac4eee1b7b6ecb127c4e4e
Ravindra <ravindrasingh0506@gmail.com>
2023-02-16 15:34:00 -0400

    last commit

--- Content.js
+++ Content.js
@@ -1,79 +1,139 @@
-// 'use strict';
 
-// const e = React.createElement;
-
-import  React, { useState } from "react";
-import { Input, value } from "./Object";
-import './Iwcn.css';
-import { Readonly } from "./Readonly";
-import {Editrow} from "./Editrow";
 
 
 function Content() {
 
-    const [useState] = React
-    // let Input = new Input()
-    // const Readonly = require("./Readonly")
-    // const Editrow = require("./Editrow")
-
+    const { useState } = React
     const [data, setData] = useState({})
     const [allData, setAllData] = useState([])
     const [edit, setEdit] = useState(Input)
     const [updateId, setUpdateId] = useState(null)
     const [index, setIndex] = useState('')
 
+    const Input = [
+        {
+            name: 'OnBoarding call',
+            value: 'ravi',
+            setValue: 'setValue1'
+        },
+        {
+            name: 'Google Search Console Access',
+            value: '',
+            setValue: 'setValue2'
+        },
+        {
+            name: 'Google Analytics Access',
+            value: '',
+            setValue: 'setValue3'
+        },
+        {
+            name: 'Website Access',
+            value: '',
+            setValue: 'setValue4'
+        },
+        {
+            name: 'Technical Audit',
+            value: '',
+            setValue: 'setValue5'
+        },
+        {
+            name: 'Anchor Text Semantic Analysis',
+            value: '',
+            setValue: 'setValue6'
+        },
+        {
+            name: 'Competitor Analysis',
+            value: '',
+            setValue: 'setValue7'
+        },
+        {
+            name: 'Anchor Text/URL Mapping',
+            value: '',
+            setValue: 'setValue8'
+        },
+        {
+            name: 'Google Data Studio Report + Local Reporting Suite',
+            value: '',
+            setValue: 'setValue9'
+        },
+        {
+            name: 'Site Level Optimization',
+            value: '',
+            setValue: 'setValue10'
+        },
+        {
+            name: 'On Page Optimisation',
+            value: '',
+            setValue: 'setValue11'
+        },
+        {
+            name: 'Content Creation',
+            value: '',
+            setValue: 'setValue12'
+        },
+        {
+            name: 'Content Publishing',
+            value: '',
+            setValue: 'setValue13'
+        },
+        {
+            name: 'Premium Press Release',
+            value: '',
+            setValue: 'setValue14'
+        },
+        {
+            name: 'Authority Niche Placements',
+            value: '',
+            setValue: 'setValue15'
+        },
+        {
+            name: 'Review Management',
+            value: '',
+            setValue: 'setValue16'
+        },
+        {
+            name: 'Index Links',
+            value: '',
+            setValue: 'setValue17'
+        },
+        {
+            name: 'Vedio Recamp',
+            value: '',
+            setValue: 'setValue18'
+        }
+    ]
+
 
     function sentValue(value, name, e) {
         let Arr = {}
-        // Arr[i] = {}
         Arr[name] = value
-        // Arr[i] = value
-        // console.log('arr', Arr)
         setData({ ...data, ...Arr })
-
-        // console.log(data)
-        // console.log(Object.values(data))
-
     }
-    // console.log(data)
+
 
     function submitForm(e) {
         e.preventDefault()
-        // let array = Object.values(data)
+
         console.log(data)
         setAllData([...allData, data])
-        // let newObj = {}
-        // setData(newObj)
         console.log(data)
     }
     console.log(data)
 
     function handleEdit(e, I) {
-        // console.log('e',e,I)
-        // console.log(I)
         setUpdateId(I)
         setEdit(e)
         setIndex(I)
-        // let index = allData.findIndex((data) => data == edit)
-        // console.log(index) 
 
     }
 
     function handleEditForm(name, value) {
-        // console.log('obj', name, value)
         let editData = {}
-        // Arr[i] = {}
         editData[name] = value
-
-        // Arr[i] = value
-        // console.log('edit', editData)
-
-        // setUpdatedData({...updatedData, ...edit ,...editData })
-        // console.log('obj', updatedData)
         setEdit({ ...edit, ...editData })
-        // console.log('edi', edit)
+
     }
-    // console.log('edi', edit)
+
 
     async function postData() {
         alert('Data is Ready to post', edit)
@@ -94,12 +154,12 @@ function Content() {
 
     function submitEditedForm(e) {
         e.preventDefault()
-        //  let index = allData.findIndex((data) => data == edit)
         console.log(e)
         let newdata = [...allData]
         newdata[index] = edit
         setAllData(newdata)
 
+        // setData({})
         setUpdateId(null)
         postData()
         console.log('helo', edit)
@@ -129,7 +189,7 @@ function Content() {
 
                                     <div id="formInput">
                                         <label htmlFor={e.name} id='label'>{e.name} :-</label>
-                                        <input type="text" name={e.name} value={data[e.name]} id={e.name} onChange={(E) => sentValue(E.target.value, e.name, e)} />
+                                        <input required type="text" name={e.name} value={data[e.name]} id={e.name} onChange={(E) => sentValue(E.target.value, e.name, e)} />
                                     </div>
 
                                 )
@@ -167,12 +227,49 @@ function Content() {
 
                                 allData.map((e, i) => {
                                     return (
-                                        <tr>
+                                        <React.Fragment>
 
                                             {
-                                                i == updateId ? <Editrow AllData={Input} editForm={handleEditForm} edit={edit} /> : <Readonly e={e} HandleEdit={handleEdit} I={i} HandleDelete={handleDelete} />
+                                                i == updateId ?
+
+                                                    <tr>
+                                                        {
+                                                            Input.map((e) =>
+                                                                <td><input type="text" name={e.name} id="editInput" value={edit[e.name]} onChange={(E) => handleEditForm(e.name, E.target.value)} /></td>
+                                                            )
+                                                        }
+                                                        <td>
+                                                            <button type='submit'>Save</button>
+                                                        </td>
+                                                    </tr>
+
+
+                                                    :
+
+                                                    <tr>
+                                                        <td>{e['OnBoarding call']}</td>
+                                                        <td>{e['Google Search Console Access']}</td>
+                                                        <td>{e['Google Analytics Access']}</td>
+                                                        <td>{e['Website Access']}</td>
+                                                        <td>{e['Technical Audit']}</td>
+                                                        <td>{e['Anchor Text Semantic Analysis']}</td>
+                                                        <td>{e['Competitor Analysis']}</td>
+                                                        <td>{e['Anchor Text/URL Mapping']}</td>
+                                                        <td>{e['Google Data Studio Report + Local Reporting Suite']}</td>
+                                                        <td>{e['Site Level Optimization']}</td>
+                                                        <td>{e['On Page Optimisation']}</td>
+                                                        <td>{e['Content Creation']}</td>
+                                                        <td>{e['Content Publishing']}</td>
+                                                        <td>{e['Premium Press Release']}</td>
+                                                        <td>{e['Authority Niche Placements']}</td>
+                                                        <td>{e['Review Management']}</td>
+                                                        <td>{e['Index Links']}</td>
+                                                        <td>{e['Vedio Recamp']}</td>
+                                                        <td><button className='btn' onClick={() => handleEdit(e, i)}>Edit</button>
+                                                            <button className='btn' onClick={() => handleDelete(e, i)}>Delete</button></td>
+                                                    </tr>
                                             }
-                                        </tr>
+                                        </React.Fragment>
 
 
                                     )
@@ -194,10 +291,4 @@ function Content() {
 
 }
 
-ReactDOM.render(
-    <div>
-        <Content />
-        <Editrow />
-        <Readonly/>
-    </div>
-    , document.querySelector('.box'))
+ReactDOM.render(<Content />, document.querySelector('.box'))
